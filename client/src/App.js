7b7d2a1aa7c708eb0036ab5c977a35583fc9bbde
Luis Carlin <luiscarlin@gmail.com>
2020-01-25 10:52:04 -0500

    have frontend talk to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  return <div>hello</div>
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:5000/')
+      .then(data => data.json())
+      .then(response => setMessage(response.key))
+  })
+
+  return <div>hello, {message}</div>
 }
 
 export default App
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const port = 5000
 
+var allowedOrigins = ['http://localhost:3000']
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+)
+
 app.get('/', (req, res) => {
-  res.send({ key: 'value' })
+  res.send({ key: 'luis' })
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
